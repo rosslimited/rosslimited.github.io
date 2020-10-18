fdbfe8feff99e5a447d3805e19a557c0c4df22bf
Joseph Ross <josephross@me.com>
2020-10-18 10:43:08 +0100

    page scroll

--- _includes/js/animate.js
+++ _includes/js/animate.js
@@ -30,7 +30,6 @@ $(window).scroll(function() {
 
 }).scroll();
 
-
 /* Colour Changing Splash */
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
@@ -240,3 +239,29 @@ function fauxClick(x, y) {
   fauxClick.pageY = y;
   document.dispatchEvent(fauxClick);
 }
+
+/* Smooth Scroll */
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
